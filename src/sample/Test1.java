226fa6cf8c86a9d19df64648378ba25751b74d84
priyanka <priyankamurugavel@gmail.com>
2021-03-08 11:21:02 +0530

    1

--- src/sample/Test1.java
+++ src/sample/Test1.java
@@ -19,22 +19,5 @@ public class Test1 {
 
 	}
 
-	@Test
-	public void b2() {
-		System.out.println("Test1 b2");	
-		System.out.println("Test2 c");	
-		System.out.println("Test2 c");	
-
-	}
 	
-	@Test
-	public void c2() {
-		System.out.println("Test1 b2");	
-	}
-	
-	@Test
-	public void c3() {
-		System.out.println("Test1 b2");	
-	}
-
 }
--- src/sample/Test2.java
+++ src/sample/Test2.java
@@ -18,28 +18,5 @@ public class Test2 {
 		System.out.println("Test2 c");	
 
 	}
-	
-	@Test
-	public void d2() {
-		System.out.println("Test2 d2");	
-		System.out.println("Test2 c");	
-		System.out.println("Test2 c");	
-
-	}
-	
-	@Test
-	public void d3() {
-		System.out.println("Test2 d2");	
-	}
-	
-	@Test
-	public void e3() {
-		System.out.println("Test2 e2");	
-	}
-	
-	@Test
-	public void g3() {
-		System.out.println("Test2 e2");	
-	}
 
 }
